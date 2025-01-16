77eb33601e7d4546e09cba522d72486c7d91cea5
ShroukAMohamed <shrouka.mohamed55@gmail.com>
2025-01-16 19:16:01 +0200

    firebase-config

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  baseApi: 'https://fakestoreapi.com/',
+  firebase: {
+    projectId: 'assessment-64d2d',
+    appId: '1:811088963028:web:a8a825c795390653d46241',
+    storageBucket: 'assessment-64d2d.firebasestorage.app',
+    apiKey: 'AIzaSyB_0RCY4M3nMyhpkm4Rpoiao3OV6SAqAck',
+    authDomain: 'assessment-64d2d.firebaseapp.com',
+    messagingSenderId: '811088963028',
+  },
+};
